c77cda2b5bc7944dc5f3cf832ec20193601e4d1f
busbyberkly <bzpeterson0901@gmail.com>
2023-09-14 22:17:22 -0600

    Milestone 1 and Milstone 2 are working

--- Wordle.py
+++ Wordle.py
@@ -1,22 +1,56 @@
 # File: Wordle.py
 
 """
-This module is the starter file for the Wordle assignment.
+author: Busbyberkly Peterson 9/11/23
+ file for the Wordle assignment.
 BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
 """
 
 import random
 
 from WordleDictionary import FIVE_LETTER_WORDS
-from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS, CORRECT_COLOR, PRESENT_COLOR
 
 def wordle():
 
     def enter_action(s):
-        gw.show_message("You have to implement this method.")
+        #converting the user input into a string and making it lowercase so that compared to the word dictionary 
+        line = str(s).lower()
+
+        #get a random word from the word dictionary
+        randomword = random.choice(FIVE_LETTER_WORDS)
+        hidden = str(randomword)
+        if line in FIVE_LETTER_WORDS:
+            gw.show_message("Good Job.")
+            #this is where we implement the colors and comparison to the hidden random word
+            # for (iCount) in range(N_COLS):
+            #     solution = hidden[iCount]
+            #     user = line[iCount]
+            #     #if the user-generated letter is the same as the letter in the solution turn the key GREEN
+            #     if user == solution:
+            #         gw.set_key_color(user, CORRECT_COLOR)
+
+
+
+        else:
+            gw.show_message("Not in word list.")
+            
+        
+    
 
     gw = WordleGWindow()
+    
+    
+    
+
+    # loop to get each letter from the random word to the letter boxes.
+    # for (iCount) in range(N_COLS):
+    #     letter = randomword[iCount]
+    #     gw.set_square_letter(0, iCount, letter)
+   
+    
     gw.add_enter_listener(enter_action)
+
 
 # Startup code
 
